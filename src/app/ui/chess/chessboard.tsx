92b5a405fdfaf04e9d15860ce744f747d53274ae
Mike7Dre <michael.drebant@gmail.com>
2024-07-11 22:06:50 +0200

    feat chess

--- src/app/ui/chess/chessboard.tsx
+++ src/app/ui/chess/chessboard.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import React from "react";
 
-export default function Chessboard() {
-    const squares:React.JSX.Element[] = [];
+const Chessboard: React.FC = () => {
+    const squares: React.ReactNode[] = [];
+
+    const handleClick = (index: number) => {
+        console.log("Square clicked:", index);
+        
+        // You can add logic here to handle the click event
+    };
 
     for (let i = 0; i < 64; i++) {
         const isEvenRow = Math.floor(i / 8) % 2 === 0;
@@ -11,17 +19,22 @@ export default function Chessboard() {
         squares.push(
             <div
                 key={i}
-                className={`w-16 h-16 ${isDark ? "bg-orange-200" : "bg-black"}`}
-            />
+                className={`w-16 h-16 relative cursor-pointer ${isDark ? "bg-orange-200" : "bg-black"}
+                `}
+                onClick={() => handleClick(i)}
+            >
+                <div className="absolute inset-0 bg-red-500 opacity-0 hover:opacity-100 transition-opacity"></div>
+            </div>
         );
     }
+
     return (
-        <>
         <div className="min-h-screen flex items-center justify-center">
-        <div className="grid grid-cols-8 grid-rows-8 gap-0 fixed border-[18px] border-orange-700"> 
-            {squares}
+            <div className="grid grid-cols-8 grid-rows-8 gap-0 fixed border-[16px] border-orange-700">
+                {squares}
+            </div>
         </div>
-        </div>
-        </>
-    )
-}
+    );
+};
+
+export default Chessboard;
