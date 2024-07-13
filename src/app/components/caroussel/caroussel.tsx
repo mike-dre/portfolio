860d5c7200b138d81b7994e10eb1d0458ee52300
Mike7Dre <michael.drebant@gmail.com>
2024-07-13 21:45:13 +0200

    feat caroussel

--- src/app/components/caroussel/caroussel.tsx
+++ src/app/components/caroussel/caroussel.tsx
@@ -3,9 +3,23 @@ import styles from "./caroussel.module.css"
 export function Caroussel() {
 
     return (
-        <div className="banner">
-            <div className="slider">
-                <div className="item"><img src="./images/dragon_1.jpg"></img></div>
+        <div className={styles.banner}>
+            <div className={styles.slider}>
+                <div className={styles.item} id={styles.item1}><img className={styles.img} src="./images/dragon_1.jpg"></img></div>
+                <div className={styles.item} id={styles.item2}><img className={styles.img} src="./images/dragon_2.jpg"></img></div>
+                <div className={styles.item} id={styles.item3}><img className={styles.img} src="./images/dragon_3.jpg"></img></div>
+                <div className={styles.item} id={styles.item4}><img className={styles.img} src="./images/dragon_4.jpg"></img></div>
+                <div className={styles.item} id={styles.item5}><img className={styles.img} src="./images/dragon_5.jpg"></img></div>
+                <div className={styles.item} id={styles.item6}><img className={styles.img} src="./images/dragon_6.jpg"></img></div>
+                <div className={styles.item} id={styles.item7}><img className={styles.img} src="./images/dragon_7.jpg"></img></div>
+                <div className={styles.item} id={styles.item8}><img className={styles.img} src="./images/dragon_8.jpg"></img></div>
+                <div className={styles.item} id={styles.item9}><img className={styles.img} src="./images/dragon_9.jpg"></img></div>
+                <div className={styles.item} id={styles.item10}><img className={styles.img} src="./images/dragon_10.jpg"></img></div>
+            </div>
+            <div className={styles.content}>
+
+            
+            <div className={styles.model}></div>
             </div>
         </div>
     )
